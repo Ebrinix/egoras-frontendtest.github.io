0dd682175f2f3ede803f71033836682670f50833
Ebrinix <121ebrinix@gmail.com>
2021-06-17 12:09:06 +0100

    Updates

---
+++ precache-manifest.1a576bad36f99467a78142e468a6d940.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f73660cb3cc056fcc5f12772492216c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b273d6ed982acd1c847",
+    "url": "/static/css/2.ebe6ab42.chunk.css"
+  },
+  {
+    "revision": "b7987bbcb75d921b9e6b",
+    "url": "/static/css/main.f69c1d72.chunk.css"
+  },
+  {
+    "revision": "0b273d6ed982acd1c847",
+    "url": "/static/js/2.1a3a9d8f.chunk.js"
+  },
+  {
+    "revision": "5ec6605fef745b2d36e28e7fa04de4f9",
+    "url": "/static/js/2.1a3a9d8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99dd8613fb3f59d8da67",
+    "url": "/static/js/3.d2d08b27.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/3.d2d08b27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3551ee7fd8bd9be86bdc",
+    "url": "/static/js/4.ca8af364.chunk.js"
+  },
+  {
+    "revision": "312b03fc2032f4a26db5e85de50ce98a",
+    "url": "/static/js/4.ca8af364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e885b70df8ae93b5a05",
+    "url": "/static/js/5.ce5fdd88.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.ce5fdd88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7987bbcb75d921b9e6b",
+    "url": "/static/js/main.58bf80f9.chunk.js"
+  },
+  {
+    "revision": "f6263e6f77dfc74c9349",
+    "url": "/static/js/runtime-main.ff4d81f3.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/egoras-frontendtest/precache-manifest.3b40c9f8b8d73e8bc23e186673c4bc0c.js"
+  "/precache-manifest.1a576bad36f99467a78142e468a6d940.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/egoras-frontendtest/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
